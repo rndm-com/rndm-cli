f7e599c6894abcfe14485d25ed6d8f2ae70f12d2
Paul Napier <>
2018-07-31 13:14:44 +1000

    Initial Commit

---
+++ src/actions/init/index.js
@@ -0,0 +1,65 @@
+require('colors');
+const fs = require('fs');
+const { noop } = require('lodash');
+const Process = require('child_process');
+const request = require('request');
+
+const current = process.cwd();
+
+const options = {
+  stdio: 'inherit'
+};
+
+const changeProjectName = (name) => {
+  const packagePath = [current, name, 'package.json'].join('/');
+  const packageJSON = require(packagePath);
+  packageJSON.name = name;
+  fs.writeFileSync(packagePath, JSON.stringify(packageJSON, null, 2));
+};
+
+const addPackages = (name, { packages = [] }, callback = noop) => {
+  request("https://rndm-com.firebaseio.com/api/packages.json", (error, response, body) => {
+    const json = JSON.parse(body);
+    const keys = Object.keys(json);
+    const available = packages.filter(p => keys.includes(p));
+    const packagePath = [current, name, 'package.json'].join('/');
+    const packageJSON = require(packagePath);
+    const { dependencies } = packageJSON;
+    available.forEach(p => {
+      const { local } = json[p];
+      dependencies[p] = local;
+    });
+    fs.writeFileSync(packagePath, JSON.stringify(packageJSON, null, 2));
+    callback();
+  });
+};
+
+const fromGit = (name, cmd, callback = noop) => {
+  request("https://rndm-com.firebaseio.com/api/template.json", (error, response, body) => {
+    const { url } = JSON.parse(body);
+    const git = Process.spawn('git', ['clone', '--depth=1', '--branch=master', url, name], options);
+    git.on('close', () => {
+      Process.exec(`rm -rf ${name}/.git`, () => {
+        changeProjectName(name);
+        addPackages(name, cmd, callback)
+      });
+    });
+  });
+};
+
+const init = (name, cmd) => {
+  fromGit(name, cmd)
+};
+
+const quickstart = (name, cmd) => {
+  fromGit(name, cmd, () => {
+    options.cwd = [current, name].join('/');
+    const install = Process.spawn('npm', ['install'], options);
+    install.on('close', () => {
+      Process.spawn(`npm`, ['run', 'web'], options)
+    });
+  });
+};
+
+module.exports.init = init;
+module.exports.quickstart = quickstart;
---
+++ src/index.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const program = require('commander');
+const { init , quickstart} = require('./actions/init');
+const example = require('./actions/example');
+const name = 'rndm-client';
+
+const split = (i = '') => i.split(',');
+
+program
+  .version('0.1.0')
+  .description('RNDM Renderer Redux Plugin');
+
+program
+  .command('init <name>')
+  .option('-p, --packages <pkgs>', 'Determines which RNDM plugins and/or presets to install with the project. Default is none.', split)
+  .action(init);
+
+program
+  .command('quickstart <name>')
+  .option('-p, --packages <pkgs>', 'Determines which RNDM plugins and/or presets to install with the project. Default is none.', split)
+  .action(quickstart);
+
+program
+  .command('example')
+  .option('-p, --package <pkg>', 'Determines which package to use')
+  .option('-t, --template <template>', 'Determines which template to use')
+  .action(example);
+
+program.parse(process.argv);
+
+module.exports.init = init;
+module.exports.quickstart = quickstart;
